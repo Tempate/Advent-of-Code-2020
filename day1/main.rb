b19e68e4364974fb79d23ac54a53db8c48ed74fe
Tempate <ddiazq28@gmail.com>
2020-12-01 22:27:47 +0100

    Recursive solution

--- day1/main.rb
+++ day1/main.rb
@@ -1,12 +1,34 @@
 file = File.open("input.txt")
 lines = file.readlines.map(&:chomp).map(&:to_i)
 
-lines.each_with_index do |n1, i|
-    lines[(i+1)..-1].each_with_index do |n2, j|
-        lines[(j+1)..-1].each do |n3|
-            if n1 + n2 + n3 == 2020 
-                puts n1 * n2 * n3
-            end
+
+def f(list, count, result)
+    if list.length() == 0
+        return nil
+    elsif count == 1
+        first = list[0]
+
+        if first == result
+            return [first]
+        elsif list.length() == 0
+            return nil
+        else
+            return f(list.drop(1), count, result)
         end
     end
+
+    first = list[0]
+    list = list.drop(1)
+
+    answer = f(list, count - 1, result - first)
+
+    if answer == nil
+        return f(list, count, result)
+    else
+        return answer.push(first)
+    end
 end
+
+
+puts "Part 1: " + f(lines, 2, 2020).inject(:*).to_s
+puts "Part 2: " + f(lines, 3, 2020).inject(:*).to_s
