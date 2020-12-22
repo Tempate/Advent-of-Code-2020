513b0eeddadb553f257d4536e2af8cb02b6f27cf
Tempate <ddiazq28@gmail.com>
2020-12-23 00:02:56 +0100

    Improved performance

--- day22/main.rb
+++ day22/main.rb
@@ -1,8 +1,7 @@
 require 'set'
 
 
-file = File.open("input.txt")
-input = file.read.split("\n\n")
+input = File.read("input.txt").split("\n\n")
 
 
 def parse(input)
@@ -24,6 +23,7 @@ def run_game2(decks)
 
     until stop_playing(decks)
         # The first player wins when reaching an already played position
+        # If the position is repeated, both their decks will be the same
         if history.include? decks[0]
             return [decks[0], []]
         end
@@ -31,12 +31,13 @@ def run_game2(decks)
         # Save decks to history
         history.add(Array.new(decks[0]))
 
+        # Take the first card from each deck
         cards = decks.map(&:shift)
 
         if play_subgame(decks, cards)
             subdecks = decks.map.with_index{ |deck, i| deck[..cards[i]-1] }
 
-            if run_game2(subdecks)[0].length > 0
+            if run_game2(subdecks).empty?
                 decks[0].push(cards[0], cards[1])
             else
                 decks[1].push(cards[1], cards[0])
@@ -61,7 +62,7 @@ end
 
 
 def stop_playing(decks)
-    decks.map(&:length).include? 0
+    decks.any?(&:empty?)
 end
 
 
@@ -71,9 +72,12 @@ end
 
 
 def score(deck)
-    deck.reverse.map.with_index{ |card, index| card * (index + 1) }.inject(:+)
+    deck.reverse.map.with_index(1){ |card, index| card * index }.inject(:+)
 end
 
 
-puts "Part 1: " + score(run_game1(parse(input)).max).to_s
-puts "Part 2: " + score(run_game2(parse(input)).max).to_s
+decks = parse(input)
+
+
+puts "Part 1: " + score(run_game1(decks.map(&:dup)).max).to_s
+puts "Part 2: " + score(run_game2(decks.map(&:dup)).max).to_s
